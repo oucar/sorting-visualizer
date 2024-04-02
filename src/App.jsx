f39f277db3abcd24de10b9666b03a3f72dbc5d4e
Onur Ucar <hello@ucaronur.com>
2024-04-02 01:56:42 -0400

    Initial bubble sort algortihm implementation with bar displaying feature

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Element from './components/element/Element';
+import bubbleSort from './algorithms/bubbleSort';
+
+const App = () => {
+
+  const [arr, setArr] = useState([20, 10, 14, 15, 7, 2, 12, 16, 17, 5])
+  const [elements, setElements] = useState()
+  bubbleSort(arr, setArr)
+
+
+  useEffect(() => {
+     setElements(arr.map((el, i) => {
+      return (
+        <Element
+          key={i}
+          orderNo={i}
+          height={el*10}
+          active={false}
+          sorted={false}
+        />
+      )
+    }))
+  }, [arr])
+
+  return (
+    <div className="App">
+      <div className='elements'>
+        {elements}
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/algorithms/bubbleSort.js
@@ -0,0 +1,31 @@
+const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
+const bubbleSort = async (
+  arr,
+  setterFunc,
+  setActiveIndex,
+  delayMilliSeconds
+) => {
+  const len = arr.length;
+  if (len <= 1) return arr;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      await delay(delayMilliSeconds);
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+      setActiveIndex([j, j + 1]);
+      // pass in new array ∵ react compares by reference
+      setterFunc([...arr]);
+    }
+  }
+
+  for (let i = 0; i < len; i++) {
+    await delay(delayMilliSeconds);
+    setActiveIndex([i]);
+  }
+  setActiveIndex([]);
+};
+
+export default bubbleSort;
