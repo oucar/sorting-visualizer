4b743f7590f1901af9dfa032e2fff39c26ec7acb
Onur Ucar <hello@ucaronur.com>
2024-04-02 21:56:23 -0400

    Added temp tooltips

--- src/components/meta-data/MetaData.jsx
+++ src/components/meta-data/MetaData.jsx
@@ -2,7 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "../../features/delaySlice";
 import "./MetaData.css";
 import padNumberWithZeros from "../../utils/padNumbersWithZeros";
-import Tooltip from '@mui/material/Tooltip';
+import Tooltip from "@mui/material/Tooltip";
+import Fade from "@mui/material/Fade";
 
 const MetaData = (props) => {
   const sortingAlgorithm = useSelector((state) => state.algorithm.value);
@@ -12,29 +13,69 @@ const MetaData = (props) => {
   return (
     // @@TODO: complete tooltips
     <div className="meta-data">
-      <p className="meta-data__algorithm">{sortingAlgorithm}</p>
-      <Tooltip title="Add" arrow>
+      <Tooltip
+        title="Add"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 300 }}
+        arrow
+      >
+        <p className="meta-data__algorithm">{sortingAlgorithm}</p>
+      </Tooltip>
+      <Tooltip
+        title="Add"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 300 }}
+        arrow
+      >
         <p>Iterations: {padNumberWithZeros(props.metaData.iterations)}</p>
       </Tooltip>
-      <p>Comparisons: {padNumberWithZeros(props.metaData.comparisons)}</p>{" "}
-      <p>Swaps: {padNumberWithZeros(props.metaData.swaps, 3)}</p>
-      <p>Shifts: {padNumberWithZeros(props.metaData.shifts, 3)}</p>
-      <p>
-        Delay: {padNumberWithZeros(delay, 3)}ms &nbsp;
-        <span
-          className="change-delay increase"
-          onClick={() => dispatch(increment())}
-        >
-          &#9650;
-        </span>
-        &nbsp;
-        <span
-          className="change-delay decrease"
-          onClick={() => dispatch(decrement())}
-        >
-          &#9660;
-        </span>
-      </p>
+      <Tooltip
+        title="Add"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 300 }}
+        arrow
+      >
+        <p>Comparisons: {padNumberWithZeros(props.metaData.comparisons)}</p>
+      </Tooltip>
+      <Tooltip
+        title="Add"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 300 }}
+        arrow
+      >
+        <p>Swaps: {padNumberWithZeros(props.metaData.swaps, 3)}</p>
+      </Tooltip>
+      <Tooltip
+        title="Add"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 300 }}
+        arrow
+      >
+        <p>Shifts: {padNumberWithZeros(props.metaData.shifts, 3)}</p>
+      </Tooltip>
+      <Tooltip
+        title="Add"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 300 }}
+        arrow
+      >
+        <p>
+          Delay: {padNumberWithZeros(delay, 3)}ms &nbsp;
+          <span
+            className="change-delay increase"
+            onClick={() => dispatch(increment())}
+          >
+            &#9650;
+          </span>
+          &nbsp;
+          <span
+            className="change-delay decrease"
+            onClick={() => dispatch(decrement())}
+          >
+            &#9660;
+          </span>
+        </p>
+      </Tooltip>
     </div>
   );
 };
