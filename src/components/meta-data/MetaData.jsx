8f39bfd351abc6281022b0174d9bca1128922968
Onur Ucar <hello@ucaronur.com>
2024-04-04 23:58:39 -0400

    Added explanations to the tooltips

--- src/components/meta-data/MetaData.jsx
+++ src/components/meta-data/MetaData.jsx
@@ -13,7 +13,7 @@ const MetaData = (props) => {
   return (
     <div className="meta-data">
       <Tooltip
-        title="Add"
+        title={"Say hi to " + sortingAlgorithm + "!"}
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 300 }}
         arrow
@@ -21,7 +21,7 @@ const MetaData = (props) => {
         <p className="meta-data__algorithm">{sortingAlgorithm}</p>
       </Tooltip>
       <Tooltip
-        title="Add"
+        title="How many times the algorithm goes through the list to sort it."
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 300 }}
         arrow
@@ -29,7 +29,7 @@ const MetaData = (props) => {
         <p>Iterations: {padNumberWithZeros(props.metaData.iterations)}</p>
       </Tooltip>
       <Tooltip
-        title="Add"
+        title="How many times the algorithm checks if one element is greater than, less than, or equal to another."
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 300 }}
         arrow
@@ -37,7 +37,7 @@ const MetaData = (props) => {
         <p>Comparisons: {padNumberWithZeros(props.metaData.comparisons)}</p>
       </Tooltip>
       <Tooltip
-        title="Add"
+        title="How many times the algorithm switches the positions of elements."
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 300 }}
         arrow
@@ -45,7 +45,7 @@ const MetaData = (props) => {
         <p>Swaps: {padNumberWithZeros(props.metaData.swaps, 3)}</p>
       </Tooltip>
       <Tooltip
-        title="Add"
+        title="How many times elements are moved to make space for others (mostly in insertion sort)."
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 300 }}
         arrow
@@ -53,7 +53,7 @@ const MetaData = (props) => {
         <p>Shifts: {padNumberWithZeros(props.metaData.shifts, 3)}</p>
       </Tooltip>
       <Tooltip
-        title="Add"
+        title="The pauses between steps in the visualization to make it easier to follow. Lower value means faster visualization."
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 300 }}
         arrow
